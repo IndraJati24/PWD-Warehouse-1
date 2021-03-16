f79411f8ccfb2ea46fa3e5e0e7ce0bd8b122336a
anas-arsyad <anasarsyad@protonmail.com>
2021-03-16 17:23:19 +0700

    PW1-16-detail product

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import { Route, Switch } from "react-router-dom";
 import "./App.css";
+
 import Navigation from "./components/Navigation";
 import Home from "./pages/Home";
+import DetailProduct from "./pages/DetailProduct";
 
 import Login from "./pages/Login";
 import register from "./pages/register";
@@ -12,7 +14,8 @@ function App() {
     <>
       <Navigation />
       <Switch>
-        <Route path="/" exact component={Home} />
+        <Route path="/" exact component={Home} exact />
+        <Route path="/detail/:id" component={DetailProduct} />
         <Route path="/login" component={Login} />
         <Route path="/register" component={register} />
         <Route path="/verification" component={Verify} />
