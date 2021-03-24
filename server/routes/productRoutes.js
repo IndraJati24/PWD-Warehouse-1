e91ea6b75c6e8549efa59fd531a788b56ab3e202
anas-arsyad <anasarsyad@protonmail.com>
2021-03-24 13:52:02 +0700

    PW1-filteruploadbukti

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -5,6 +5,7 @@ const { productController } = require("../controllers");
 router.get("/getAll", productController.getAllProduct);
 router.get("/getCategory", productController.getAllCategory);
 router.get("/getDetailPage/:id",productController.getDetailPage)
+router.get("/getCarousel", productController.getCarousel);
 
 
 module.exports = router;
